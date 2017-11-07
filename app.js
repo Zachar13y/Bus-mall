4dc8d7e4ee76bcaa693b99cea0b795fa906e3ef5
Zachar13y <zachar13y@gmail.com>
2017-11-07 11:06:58 -0800

    made image paths into strings, modified constructor function

--- app.js
+++ app.js
@@ -3,45 +3,47 @@
 // let myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 'use strict';
 
-const bag = new Pic ('bag', imgs/bag.jpeg);
-const banana = new Pic ('banana', imgs/bag.jpg);
-const bathroom = new Pic ('bathroom', imgs/bathroom.jpg);
-const boots = new Pic ('boots', imgs/boots.jpg);
-const breakfast = new Pic ('breakfast', imgs/breakfast.jpg);
-const bubblegum = new Pic ('bubblegum', imgs/bubblegum.jpg);
-const chair = new Pic ('chair', imgs/chair.jpg);
-const cthulhu = new Pic ('cthulhu', imgs/cthulhu.jpg);
-const dogDuck = new Pic ('dogDuck', imgs/dog-duck.jpg);
-const dragon = new Pic ('dragon', imgs/dragon.jpg);
-const pen = new Pic ('pen', imgs/pen.jpg);
-const pet = new Pic ('pet', imgs/pet.jpg);
-const scissors = new Pic ('scissors', imgs/scissors.jpg);
-const shark = new Pic ('shark', imgs/shark.jpg);
-const sweep = new Pic ('sweep', imgs/sweep.jpg);
-const tauntaun = new Pic ('tauntaun', imgs/tauntaun.jpg);
-const unicorn = new Pic ('unicorn', imgs/tauntaun.jpg);
-const usb = new Pic ('usb', imgs/usb.gif);
-const water = new Pic ('water', imgs/water.jpg);
-const wine = new Pic ('wine', imgs/wine.jpg);
+const bag = new Pic ('bag', 'imgs/bag.jpeg');
+const banana = new Pic ('banana', 'imgs/bag.jpg');
+const bathroom = new Pic ('bathroom', 'imgs/bathroom.jpg');
+const boots = new Pic ('boots', 'imgs/boots.jpg');
+const breakfast = new Pic ('breakfast', 'imgs/breakfast.jpg');
+const bubblegum = new Pic ('bubblegum', 'imgs/bubblegum.jpg');
+const chair = new Pic ('chair', 'imgs/chair.jpg');
+const cthulhu = new Pic ('cthulhu', 'imgs/cthulhu.jpg');
+const dogDuck = new Pic ('dogDuck', 'imgs/dog-duck.jpg');
+const dragon = new Pic ('dragon', 'imgs/dragon.jpg');
+const pen = new Pic ('pen', 'imgs/pen.jpg');
+const pet = new Pic ('pet', 'imgs/pet.jpg');
+const scissors = new Pic ('scissors', 'imgs/scissors.jpg');
+const shark = new Pic ('shark', 'imgs/shark.jpg');
+const sweep = new Pic ('sweep', 'imgs/sweep.jpg');
+const tauntaun = new Pic ('tauntaun', 'imgs/tauntaun.jpg');
+const unicorn = new Pic ('unicorn', 'imgs/tauntaun.jpg');
+const usb = new Pic ('usb', 'imgs/usb.gif');
+const water = new Pic ('water', 'imgs/water.jpg');
+const wine = new Pic ('wine', 'imgs/wine.jpg');
 
-const picArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'pet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water', 'wine'];
+const picArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, pet, scissors, shark, sweep, tauntaun, unicorn, usb, water, wine];
 
-function appendRandomPic(); 
+console.log (picArray);
+// function appendRandomPic(){
 
-for(let i = 0; i < 3; i++) {
-    const game = document.getElementById('game');
-    randomPicc = picArray[ Math.floor(Math.random() * 20)];
+//     for(let i = 0; i < 3; i++) {
+//     const game = document.getElementById('game');
+//     randomPicc = picArray[ Math.floor(Math.random() * 20)];
 
-}
+// }
+// }
 
 
-function picGame (name, src) {
+function Pic (name, src) {
     this.type = 'type';
-    this.src = './imgs/' + src;
+    this.src = src;
     this.picked = 0;
 }
 
-picGame.prototype.wasPicked = function () {
-    this.picked += 1;
-};
+// pic.prototype.render = function () {
+//     const ele =
+// };
 
