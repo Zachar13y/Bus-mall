a551e81b1809d726877c7a2afc4de37f33610b33
Zachar13y <zachar13y@gmail.com>
2017-11-10 14:45:33 -0800

    Added text and image to header. Added margins and padding to all. Added borders to survey images. Added backgrounds.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-// const imageArray = ['imgs/bathroom.jpg', 'imgs/boots.jpg', 'imgs/breakfast.jpg', 'imgs/bubblegum.jpg', 'imgs/chair.jpg', 'imgs/cthulhu.jpg', 'imgs/dog-duck.jpg', 'imgs/dragon.jpg', 'imgs/pen.jpg', 'imgs/pet-sweep.jpg', 'imgs/scissors.jpg', 'imgs/shark.jpg', 'imgs/sweep.jpg', 'imgs/tauntaun.jpg', 'imgs/unicorn.jpg', 'imgs/usb.gif', 'imgs/water-can.jpg', 'imgs/wine-glass.jpg'];
-// let myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 'use strict';
 
 const bag = new Pic ('bag', 'imgs/bag.jpg');
@@ -26,32 +23,34 @@ const wine = new Pic ('wine', 'imgs/wine-glass.jpg');
 
 const picArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, pet, scissors, shark, sweep, tauntaun, unicorn, usb, water, wine];
 
+//function declaration declaring properties and values
 function Pic (type, src) {
     this.type = type;
     this.src = src;
     this.picked = 0;
-};
+}
+//function expression expressing that it will render a prototype of a Pic.
+//create image element
+//give the element some stuff including src
+//return element
 Pic.prototype.render = function () {
     const ele = document.createElement('img');
     ele.src = this.src;
     return ele;
 };
-//create image element
-//give the element some stuff including src
-//return element
 
-// const ele = document.createElement('Pic'['type', 'src']);
+
 for (let i = 0; i < 3; i++){
     appendRandomPic();
 }
 
-const show = document.getElementById('show');
+const survey = document.getElementById('survey');
 
-function clickHandler (e) {
+function clickHandler (e){
     const clickedPic = e.target;
-
-    if (clickedPic.id === 'show') return;
-
+    if (clickedPic.id === 'survey'){
+        return;
+    }
 
     for (let i = 0; i < picArray.length; i++){
         const PicClass = clickedPic.classList.value;
@@ -59,20 +58,53 @@ function clickHandler (e) {
             picArray[i].wasClicked();
             console.log('Number of clicks', picArray[i].clicked);
         }
+        //to remove three pictures on each click  get an array of all html elements loop through three items in a pic array remove each element in that array.
+        const survey = document.getElementById('survey');
+
+        while(survey.firstChild){
+            survey.removeChild(survey.firstChild);
+        }
     }
-    clickedPic.remove();
+    document.querySelectorAll('img');
+    const images = document.querySelectorAll('img');
+    console.log (images);
+
+
+    for (let i = 0; i < 3; i++){
+        appendRandomPic();
+    }
 };
 
-show.addEventListener('click', clickHandler);
+survey.addEventListener('click', clickHandler);
 
 function appendRandomPic (){
-    const show = document.getElementById('show');
+    const survey = document.getElementById('survey');
     const randomPic = picArray[ Math.floor(Math.random() * picArray.length)];
     const randomPicEle = randomPic.render();
 
-    show.appendChild(randomPicEle);
+    survey.appendChild(randomPicEle);
 }
 
-clickHandler();
-// appendRandomPic();
-console.log (picArray);
+// function shuffleArray(picArray) {
+//     let i = picArray.length - 1;
+//     for (; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       const temp = picArray[i];
+//       picArray[i] = picArray[j];
+//       picArray[j] = temp;
+//     }
+//     return picArray;
+// }
+// console.log (picArray);
+
+// function shuffleArray()
+
+
+
+
+// function appendRandomPic () {
+//     const tempArray = [];
+//     const select = document.getElementById('select');
+//     for (let i = 0; i < 3; i++){
+//         const randomPic = pics[Math.floor(Math.random())];localStorage;
+//
